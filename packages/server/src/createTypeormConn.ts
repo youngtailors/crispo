009c8fa9b02041a0b94d5cfefda8542f7d2ebf54
Evan N <dacsang97@gmail.com>
2019-01-03 21:55:19 +0700

    feat(server) - fix not found ormconfig

--- packages/server/src/createTypeormConn.ts
+++ packages/server/src/createTypeormConn.ts
@@ -2,9 +2,5 @@ import { createConnection, getConnectionOptions } from 'typeorm'
 
 export const createTypeormConn = async () => {
   const config = await getConnectionOptions(process.env.NODE_ENV)
-
-  return createConnection({
-    ...config,
-    name: 'default',
-  })
+  return createConnection(config)
 }
