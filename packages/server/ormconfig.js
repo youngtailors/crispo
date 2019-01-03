d610a694e3f6fdd5a4045071db569daee551dd5c
Evan N <dacsang97@gmail.com>
2019-01-03 22:37:14 +0700

    feat(server) - nohoist typeorm

--- packages/server/ormconfig.js
+++ packages/server/ormconfig.js
@@ -1,3 +1,22 @@
-const config = require('../../ormconfig')
+require('dotenv-safe').config()
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = config
+module.exports = {
+  name: process.env.NODE_ENV,
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [`src/entities/*.ts`],
+  subscribers: [`src/subscribers/*.ts`],
+  migrations: [`src/migrations/*.ts`],
+  synchronize: !isProd,
+  logging: !isProd,
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+    subscribersDir: 'src/subscribers',
+  },
+}
